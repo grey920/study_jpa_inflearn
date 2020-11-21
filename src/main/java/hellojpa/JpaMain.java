a9dcd405b9cf86b7845a8acea12471db3ccfb53e
gwjung <charminggw@gmail.com>
2020-11-21 22:13:07 +0900

    JPA를 이용한 회원 수정하기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,11 +20,15 @@ public class JpaMain {
         /* 정석코드 : 문제가 발생해도 close할 수 있도록 try-catch 사용하기 */
         try {
             /* 회원 수정 */
-            // 엔티티 매니저를 마치 자바 컬렉션처럼 내 객체를 대신 저장해주는 것이라 생각하자. 
             Member findMember = em.find(Member.class, 1L); // 대상 엔티티 클래스, PK
-            System.out.println("findMember.id = "+ findMember.getId());
-            System.out.println("findMember.name = "+ findMember.getName());
+            findMember.setName("HelloJPA");
             
+            // 수정할 때 em.persist(findMember); 안해도 된다 -> 마치 자바 컬렉션처럼 설계되었기 때문에! setter로 끝남
+            
+            /* JPA를 통해 엔티티를 가져오면 JPA가 그걸 관리하기 시작한다 (findMember)
+             * 변경이 생겼다 판단되면 트랜잭션 커밋하기 직전에 update 쿼리를 만들어서 날리고 트랜잭션이 커밋된다. 
+             * */
+          
             tx.commit(); // 정상적일때 커밋
         } catch (Exception e) {
             tx.rollback(); // 문제가 생기면 롤백을 한다.
