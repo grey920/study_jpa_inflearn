4b5226b5932f6870e05a906ecd532d20518d272f
gwjung <charminggw@gmail.com>
2020-11-23 08:26:24 +0900

    1차 캐시에서 조회

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,22 +18,11 @@ public class JpaMain {
         tx.begin(); 
 
         try {
-        	// 준영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
+        	
+            // 조회 쿼리가 한 번만 나간다
+            Member findMember1 = em.find(Member.class, 101L); // 조회쿼리 o
+            Member findMember2 = em.find(Member.class, 101L); // 조회쿼리 x
             
-            // 영속
-            System.out.println("=== BRFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
-            
-            // 조회하기 - 조회용 sql이 나가는지가 중요하다 -> 안나감!! 왜?? 
-            // -> em.persist(member)에서 1차 캐시에 저장되고, 똑같은 PK로 조회하기 때문에 1차 캐시에서 바로 조회한 것!
-            Member findMember = em.find(Member.class, 101L);
-            
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
             tx.commit(); 
         } catch (Exception e) {
             tx.rollback(); 
