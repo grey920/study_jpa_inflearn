518a71a5e06fe04ef63c36a028389029f826857b
gwjung <charminggw@gmail.com>
2020-11-24 06:09:26 +0900

    플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,7 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-
+/* 플러시
+ * 영속성 컨텍스트의 변경사항과 데이터베이스를 맞추는 작업
+ * */
 public class JpaMain {
 
 	public static void main(String[] args) {
@@ -18,18 +20,14 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			/* 버퍼링 모아서 write 하기 */
-
 			// 영속
-			Member member1 = new Member(150L, "A");
-			Member member2 = new Member(160L, "B");
-
-			// 영속성 컨텍스트에 쿼리가 차곡차곡 쌓인다
-			em.persist(member1);
-			em.persist(member2);
-
+			
+			Member member = new Member(200L, "member200");
+			em.persist(member); // 영속성 컨텍스트에 담긴다.
+			
+			em.flush(); // 데이터베이스에 쿼리가 이 시점에 바로 나간다. (강제호출)
+			
 			System.out.println("=============================");
-			// 커밋 시점에 쿼리가 데이터베이스에 한번에 나간다. 왜 이렇게 하는거지?? -> [버퍼링]!!
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
