898b7f5478fe5b172a94928b019f83c5a93e36b6
gwjung <charminggw@gmail.com>
2020-11-24 06:19:35 +0900

    준영속 상태 (detach, clear)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,11 +21,13 @@ public class JpaMain {
 
 		try {
 			// 영속
+			Member member = em.find(Member.class, 150L);
+			member.setName("AAAAA"); // dirty checking -> update
 			
-			Member member = new Member(200L, "member200");
-			em.persist(member); // 영속성 컨텍스트에 담긴다.
+			//em.detach(member); // 이제 JPA에서 관리하지 않음 (트랜잭션을 커밋할 때 아무일도 일어나지 않는다. setName했어도 업데이트x)
+			em.clear(); // 영속성 컨텍스트 안을 통채로 날리는 것
 			
-			em.flush(); // 데이터베이스에 쿼리가 이 시점에 바로 나간다. (강제호출)
+			Member member2 = em.find(Member.class, 150L); // 1차 캐시에 다시 올리기 때문에 쿼리가 또 나간다 (test case 작성하고 싶을떄, 눈으로 보고 싶을 떄)
 			
 			System.out.println("=============================");
 			tx.commit();
