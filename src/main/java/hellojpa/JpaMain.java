2cafd1b46470cb5376bb52ed58818ee5239c12a5
gwjung <charminggw@gmail.com>
2020-11-27 06:40:34 +0900

    단방향 연관관계 - 객체지향적 모델링

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-/* 객체를 테이블에 맞추어 모델링 - 외래키 식별자를 직접 다룸*/
+
 public class JpaMain {
 
 	public static void main(String[] args) {
@@ -18,24 +18,33 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			// 팀 생성
+			// 저장
 			Team team = new Team();
 			team.setName("TeamA");
 			em.persist(team);
 			
-			// 멤버생성
 			Member member = new Member();
 			member.setUsername("member1");
-			
 			// 멤버를 팀에 소속시킨다.
-			member.setTeamId(team.getId()); // 이게 좀 객체지향스럽지 않다. 
+			member.setTeam(team); //team을 바로 넣으면 jpa가 알아서 PK값을 꺼내서 외래키로 사용함
 			em.persist(member);
+			
+			/* DB로 나가는 쿼리를 눈으로 확인하고 싶다면 - 싱크 맞추고 초기화*/
+			em.flush();
+			em.clear();
 		
+			// 조회
 			Member findMember = em.find(Member.class, member.getId());
 			
-			//찾아온 멤버의 소속을 알고싶을 때 - 연관관계가 없기 때문에 JPA에게 계속 물어봐야 한다.
-			Long findTeamId = findMember.getTeamId();
-			Team findTeam = em.find(Team.class, findTeamId);
+			//찾아온 멤버의 소속을 알고싶을 때 - 팀을 바로 끄집어내서 쓸 수 있다.
+			Team findTeam = findMember.getTeam();
+			System.out.println("findTeam = " + findTeam.getName());
+			
+			// 수정
+			// 100번 팀이 있다는 가정하에 가져와서 -> member의 팀을 setTeam으로 바꾸면
+			// DB의 외래키값이 업데이트 된다.
+			Team newTeam = em.find(Team.class, 100L);
+			findMember.setTeam(newTeam);
 			
 			tx.commit();
 		} catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,7 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
+
+import org.hibernate.annotations.ManyToAny;
 
 @Entity
 public class Member {
@@ -19,9 +23,14 @@ public class Member {
     @Column(name ="USERNAME") 
     private String username; 
    
-    // 중요!! 
-    @Column(name="TEAM_ID") //디비 테이블에 맞춘 설계
-    private Long teamId;
+//    // 중요!! 
+//    @Column(name="TEAM_ID") //디비 테이블에 맞춘 설계
+//    private Long teamId;
+    
+    /* 관계가 뭔지와 이 관계를 할 때 조인하는 컬럼이 뭔지만 알려주면 단방향 연관관계는 끝*/
+    @ManyToOne // 멤버입장에서는 Many 
+    @JoinColumn(name = "TEAM_ID") // join해야 하는 컬럼이 뭔지 - erd에서 FK로 조인된 부분
+    private Team team;
 
 	public Long getId() {
 		return id;
@@ -39,14 +48,14 @@ public class Member {
 		this.username = username;
 	}
 
-	public Long getTeamId() {
-		return teamId;
+	public Team getTeam() {
+		return team;
 	}
 
-	public void setTeamId(Long teamId) {
-		this.teamId = teamId;
+	public void setTeam(Team team) {
+		this.team = team;
 	}
     
     
-    
+
 }
