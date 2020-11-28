3931ccb56ae8ec9ebef83d31818653edc8c5139e
gwjung <charminggw@gmail.com>
2020-11-28 17:47:02 +0900

    양방향 연관관계와 연관관계의 주인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -26,27 +26,19 @@ public class JpaMain {
 			
 			Member member = new Member();
 			member.setUsername("member1");
-			// 멤버를 팀에 소속시킨다.
-			member.setTeam(team); //team을 바로 넣으면 jpa가 알아서 PK값을 꺼내서 외래키로 사용함
+			member.setTeam(team); 
 			em.persist(member);
 			
 			/* DB로 나가는 쿼리를 눈으로 확인하고 싶다면 - 싱크 맞추고 초기화*/
 			em.flush();
 			em.clear();
 		
-			// 조회
 			Member findMember = em.find(Member.class, member.getId());
-			System.out.println(member.getId());
+			List<Member> members = findMember.getTeam().getMembers();
 			
-			//찾아온 멤버의 소속을 알고싶을 때 - 팀을 바로 끄집어내서 쓸 수 있다.
-			Team findTeam = findMember.getTeam();
-			System.out.println("findTeam = " + findTeam.getName());
-			
-			// 수정
-			// 100번 팀이 있다는 가정하에 가져와서 -> member의 팀을 setTeam으로 바꾸면
-			// DB의 외래키값이 업데이트 된다.
-//			Team newTeam = em.find(Team.class, 100L);
-//			findMember.setTeam(newTeam);
+			for (Member m : members) {
+				System.out.println("m = "+ m.getUsername());
+			}
 			
 			tx.commit();
 		} catch (Exception e) {
--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -1,10 +1,14 @@
 package hellojpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+import javax.persistence.OneToMany;
+/* 양방향 연관관계 */
 @Entity
 public class Team {
 	
@@ -13,6 +17,11 @@ public class Team {
 	@Column(name = "TEAM_ID")
 	private Long id;
 	private String name;
+	
+	/* OneToMany는 읽기만 가능한 가짜 매핑(주인의 반대편),  "외래키가 있는 곳을 주인으로 정해라" */
+	@OneToMany(mappedBy = "team")//지금 일대다 매핑에서 뭐와 연결되어 있는지, 나의 반대편 사이트에 뭐가 걸려있는지 적어준다 (Member클래스의 team)
+	private List<Member> members = new ArrayList<Member>(); // add할때 null point가 안뜨게 하기 위해 관례상 arrayList로 초기화를 많이 쓴다.
+	
 	public Long getId() {
 		return id;
 	}
@@ -24,6 +33,12 @@ public class Team {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public List<Member> getMembers() {
+		return members;
+	}
+	public void setMembers(List<Member> members) {
+		this.members = members;
 	}
 	
 	
