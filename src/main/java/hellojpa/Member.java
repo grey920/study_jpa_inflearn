53c152e7680dd60c6334672befca37a00f1117b9
gwjung <charminggw@gmail.com>
2020-11-30 17:35:35 +0900

    일대다 연관관계 매핑 예시

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -22,15 +22,7 @@ public class Member {
     
     @Column(name ="USERNAME") 
     private String username; 
-   
-//    // 중요!! 
-//    @Column(name="TEAM_ID") //디비 테이블에 맞춘 설계
-//    private Long teamId;
-    
-    /* 진짜 매핑 - 연관관계의 주인 (외래 키가 있는 곳을 주인으로 정한다) */
-    @ManyToOne 
-    @JoinColumn(name = "TEAM_ID") // join해야 하는 컬럼이 뭔지 - erd에서 FK로 조인된 부분
-    private Team team;
+ 
 
 	public Long getId() {
 		return id;
@@ -47,28 +39,5 @@ public class Member {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-	public Team getTeam() {
-		return team;
-	}
-
-	public void setTeam(Team team) {
-		this.team = team;
-	}
-
-	@Override
-	public String toString() {
-		return "Member [id=" + id + ", username=" + username + ", team=" + team + "]";
-	}
-
-	// 관례적인 getter/setter가 아닌 중요한 일이란 느낌이 들게 메소드 명을 setOOO가 아닌 다른 이름으로 짓는다.
-//	public void changeTeam(Team team) {
-//		this.team = team;
-//		// 연관관계 편의 메소드 - 양쪽에 값 셋팅
-//		team.getMembers().add(this);
-//		// 여기서 기존에 있던걸 뺀다거나 null 체크도 해줄 수 있음.
-//	}
-    
-    
 
 }
--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 /* 양방향 연관관계 */
 @Entity
@@ -18,16 +19,12 @@ public class Team {
 	private Long id;
 	private String name;
 	
-	/* OneToMany는 '읽기'만 가능한 가짜 매핑(주인의 반대편),  "외래키가 있는 곳을 주인으로 정해라" */
-	@OneToMany(mappedBy = "team")//지금 일대다 매핑에서 뭐와 연결되어 있는지, 나의 반대편 사이트에 뭐가 걸려있는지 적어준다 (Member클래스의 team)
-	private List<Member> members = new ArrayList<Member>(); // add할때 null point가 안뜨게 하기 위해 관례상 arrayList로 초기화를 많이 쓴다.
+	/* 일대다관계 - 여기서 외래키를 관리한다 */
+	@OneToMany
+	@JoinColumn(name="TEAM_ID")
+	private List<Member> members = new ArrayList<Member>(); 
 	
-	// 연관관계 편의 메소드
-	public void addMember(Member member) {
-		member.setTeam(this);
-		members.add(member);
-	}
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -43,18 +40,6 @@ public class Team {
 	public List<Member> getMembers() {
 		return members;
 	}
-	public void setMembers(List<Member> members) {
-		this.members = members;
-	}
-
-	// 무한루프 방지를 위해 toString()에서  members 삭제
-	@Override
-	public String toString() {
-		return "Team [id=" + id + ", name=" + name+"]";
-	}
-	
-	
-	
 	
 	
 }
