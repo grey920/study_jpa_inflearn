5c345e303286058c74fd65afeb81fa9582b92fb3
gwjung <charminggw@gmail.com>
2020-12-05 17:27:08 +0900

    프록시의 초기화는 딱 한번만 이루어진다

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -27,10 +27,9 @@ public class JpaMain {
 			// 영속성 컨텍스트가 깔끔하게 초기화됨
 			
 //			Member findMember = em.find(Member.class, member.getId());
-			Member findMember = em.getReference(Member.class, member.getId());  //getReference 호출 시점에는 select쿼리가 안나감. 그런데 값이 실제 사용되는 시점에서 나감
-			System.out.println("findMember = "+ findMember.getClass());
-			System.out.println("findmember.id = " + findMember.getId()); // 쿼리가 안나감.-> reference를 찾을때 이미 id값을 넣어줬기 때문에 필요없다.
-			System.out.println("findmember.username = " + findMember.getUsername()); //username은 DB에 있고 영속성컨텍스트엔 없기 때문에 JPA가 없다고 판단하고 select쿼리를 날림
+			Member findMember = em.getReference(Member.class, member.getId());  
+			System.out.println("findmember.username = " + findMember.getUsername()); //내부적으로 영속성 컨텍스트에 요청해 (초기화 요청) 실제 값을 가져오게 됨
+			System.out.println("findmember.username = " + findMember.getUsername()); // 두번째부터는 초기화를 통해 target에 값이 있으므로 바로 값을 출력
 //			
 			tx.commit();
 		} catch (Exception e) {
